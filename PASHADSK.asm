88353b6102c0f21b5be939b90a7bd78343fd7db2
pppopcurn <121326462+pppopcurn@users.noreply.github.com>
2022-12-24 17:15:44 +0700

    Add files via upload

---
+++ PASHADSK.asm
@@ -0,0 +1,137 @@
+.model small
+.stack 100h
+.data
+    BarisBaru db 10,13,'$'
+    penyimpanan db 100 dup("$")
+    info db 10,13,'$'
+    tabel db 10,13,'| Nama | NIK | Nomor Kartu | Kelas Rawat |$'
+    nama db 10,13,'Masukkan Nama : $'
+    nik db 10,13,'Masukkan NIK : $'     
+    nomor_kartu db 10,13,'Masukkan Nomor Kartu : $' 
+    kelas_rawat db 10,13,'Masukkan Kelas Rawat : $'
+    
+    
+.code
+main proc
+    mov ax, @data
+    mov ds, ax
+    
+    mov si, offset penyimpanan
+    mov dx, offset nama
+    mov ah, 9
+    int 21h
+  
+nikCheck:
+    mov ah, 1
+    int 21h
+    cmp al, 13
+    je nikShow
+    mov [si], al
+    inc si
+    jmp nikCheck
+        
+nikShow:
+    mov al, 32
+    mov [si], al
+    inc si                
+        
+    mov dx, offset BarisBaru
+    mov ah, 9
+    int 21h 
+    
+    mov dx, offset nik
+    mov ah, 9
+    int 21h  
+    jmp nikSimpan
+    
+nikSimpan:
+    mov ah, 1
+    int 21h
+    cmp al, 13
+    je nomorkartuCheck
+    mov [si], al
+    inc si
+    jmp nikSimpan 
+    
+nomorkartuCheck:
+    je nomorkartuShow
+    mov [si], al
+    inc si
+    jmp nomorkartuCheck
+        
+nomorkartuShow:
+    mov al, 32
+    mov [si], al
+    inc si                
+        
+    mov dx, offset BarisBaru
+    mov ah, 9
+    int 21h 
+    
+    mov dx, offset nomor_kartu
+    mov ah, 9
+    int 21h  
+    jmp nomorkartuSimpan
+    
+nomorkartuSimpan:
+    mov ah, 1
+    int 21h
+    cmp al, 13
+    je kelasrawatCheck
+    mov [si], al
+    inc si
+    jmp nomorkartuSimpan  
+    
+
+kelasrawatCheck:
+    je kelasrawatShow
+    mov [si], al
+    inc si
+    jmp kelasrawatCheck
+        
+kelasrawatShow:
+    mov al, 32
+    mov [si], al
+    inc si                
+        
+    mov dx, offset BarisBaru
+    mov ah, 9
+    int 21h 
+    
+    mov dx, offset kelas_rawat
+    mov ah, 9
+    int 21h  
+    jmp kelasrawatSimpan
+    
+kelasrawatSimpan:
+    mov ah, 1
+    int 21h
+    cmp al, 13
+    je print
+    mov [si], al
+    inc si
+    jmp kelasrawatSimpan    
+    
+    
+print:
+    mov dx, offset info
+    mov ah, 9
+    int 21h
+                
+    mov dx, offset BarisBaru
+    mov ah, 9
+    int 21h
+                
+    mov dx, offset tabel
+    mov ah, 9
+    int 21h
+                
+    mov dx, offset BarisBaru
+    mov ah, 9
+    int 21h
+                
+    mov dx, offset penyimpanan
+    mov ah, 9
+    int 21h
+    mov ah, 4ch
+    int 21h
